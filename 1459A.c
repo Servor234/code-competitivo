656c6352f48aaa593b770db3e648c28b0bfe10f8
Servor234 <teixeirarenato603@gmail.com>
2026-05-05 15:56:01 -0300

    Fix data types and improve loop conditions

--- 1459A.c
+++ 1459A.c
@@ -1,37 +1,34 @@
 #include <stdio.h>
 
 int main(){
-    int it,Az=0,Ver=0,Nver,Naz,Di,Taz[1000],Tver[1000], cou=0;
+    int it,Az=0,Ver=0,Di,Taz[1000],Tver[1000], cou=0;
+    long long Nver,Naz;
 
     scanf("%d", &it);
 
     while(it>0){
-     scanf("%d %d %d", &Di, &Nver, &Naz);
+     scanf("%d %lld %lld", &Di, &Nver, &Naz);
 
-     while(Nver>0){
+     cou=Di-1;
+     while(cou>=0){
         Tver[cou]=Nver%10;
+        cou--;
         Nver/=10;
-        cou++;
      }
-     cou=0;
+     cou=Di-1;
 
-     while(Naz>0){
+     while(cou>=0){
         Taz[cou]=Naz%10;
-        printf("%d\n", Taz[cou]);
+        cou--;
         Naz/=10;
-        cou++;
      }
      cou=0;
 
-     for(int i=Di-1; i>0;i--){
+     for(int i=Di-1; i>=0;i--){
         if(Taz[i]>Tver[i]){
-            printf("%d %d %d\n", Taz[i], Tver[i], i);
             Az++;
         }else if(Tver[i]>Taz[i]){
-            printf("%d %d %d\n", Tver[i], Taz[i], i);
             Ver++;
-        }else{
-            printf("%d %d %d\n", Tver[i], Taz[i], i);
         }
      }
      //09281
@@ -40,7 +37,7 @@ int main(){
         printf("BLUE\n");
      }else if(Ver>Az){
         printf("RED\n");
-     }else{
+     }else if(Ver==Az){
         printf("EQUAL\n");
      }
 
@@ -48,4 +45,6 @@ int main(){
      Ver=0;
      it--;
     }
+}
+
 }
